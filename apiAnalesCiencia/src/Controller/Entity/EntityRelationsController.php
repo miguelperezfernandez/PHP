97d4fdefe45fcb2ed1fdab24933e2a240ae62239
miguelperezfernandez <miguel.perez.fernandez@alumnos.upm.es>
2022-05-03 12:51:18 +0200

    Entity class finished

--- apiAnalesCiencia/src/Controller/Entity/EntityRelationsController.php
+++ apiAnalesCiencia/src/Controller/Entity/EntityRelationsController.php
@@ -56,7 +56,11 @@ final class EntityRelationsController extends ElementRelationsBaseController
      */
     public function getPersons(Request $request, Response $response, array $args): Response
     {
-        // @TODO
+        $elementData = [
+            'getter' => 'getPersons',
+            'stuff' => PersonController::getEntitiesTag(),
+        ];
+        return $this->getElements($response, $args, $elementData);
     }
 
     /**
@@ -72,7 +76,13 @@ final class EntityRelationsController extends ElementRelationsBaseController
      */
     public function operationPerson(Request $request, Response $response, array $args): Response
     {
-        // @TODO
+        $elementData = [
+            'stuffEName' => PersonController::getEntityClassName(),
+            'stuffId' => $args['stuffId'],
+            'getter' => 'getPersons',
+            'stuff' => PersonController::getEntitiesTag(),
+        ];
+        return $this->operationStuff($request, $response, $args, $elementData);
     }
 
     /**
@@ -86,7 +96,11 @@ final class EntityRelationsController extends ElementRelationsBaseController
      */
     public function getProducts(Request $request, Response $response, array $args): Response
     {
-        // @TODO
+        $elementData = [
+            'getter' => 'getProducts',
+            'stuff' => ProductController::getEntitiesTag(),
+        ];
+        return $this->getElements($response, $args, $elementData);
     }
 
     /**
@@ -101,6 +115,12 @@ final class EntityRelationsController extends ElementRelationsBaseController
      */
     public function operationProduct(Request $request, Response $response, array $args): Response
     {
-        // @TODO
+        $elementData = [
+            'stuffEName' => ProductController::getEntityClassName(),
+            'stuffId' => $args['stuffId'],
+            'getter' => 'getProducts',
+            'stuff' => ProductController::getEntitiesTag(),
+        ];
+        return $this->operationStuff($request, $response, $args, $elementData);
     }
 }
